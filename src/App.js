2f0b4d65c510b4cd660c3954eeb7dafec3b3f6bb
Adam Szaruga <adamszaruga@gmail.com>
2018-07-12 20:45:46 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import './App.css'
+import MemoryGame from './MemoryGame.js';
+import Ajax from './Ajax.js';
+
+class App extends Component {
+
+	render() {
+
+		return (
+			<div>
+			    <div className="navbar">
+			    	<Link to="/memory">Memory Game</Link>
+			    	<Link to="/ajax">AJAX</Link>
+			    </div>
+			    <Switch>
+				    <Route path="/memory" component={MemoryGame} />
+				    <Route path="/ajax" component={Ajax} />
+				    <Route path="/" component={MemoryGame} />
+			    </Switch>
+		    </div>
+
+		);
+	}
+}
+
+export default App;
---
+++ src/MemoryCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './MemoryCard.css'
+
+class MemoryCard extends Component {
+
+	render() {
+		var cardClass = "MemoryCardInner";
+		if (this.props.isFlipped) {
+		    cardClass += " flipped";
+		}
+		return (
+		    <div className="MemoryCard" onClick={this.props.pickCard}>
+		        <div className={cardClass}>
+		            <div className="MemoryCardBack">
+		                <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" />
+		            </div>
+		            <div className="MemoryCardFront">{this.props.symbol}</div>
+		        </div>
+		    </div>
+		);
+	}
+}
+
+export default MemoryCard;
